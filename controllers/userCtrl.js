1654fb9f33eaf8da8874ab22817c10630786ea3e
akashahmed-coder <86545072+akashahmed-coder@users.noreply.github.com>
2022-01-02 11:49:12 +0600

    m

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken')
 const userCtrl = {
     register: async(req,res)=>{
         try{
-<<<<<<< HEAD
            const {name,email,password} = req.body
           const user = await User.findOne({email})
           if(user){
@@ -21,41 +20,10 @@ const userCtrl = {
           
         }catch(err){
          res.json(err.message)
-=======
-            const {name,email,password} = req.body
-            if( !name || !email || !password){
-                return res.status(422).json({error:"please filled the input field properly"})
-            }
-            const userExist = await User.findOne({email:email})
-            if(userExist){
-               return res.status(400).json({error:"Email has already present"})
-            }
-           if(password.length < 6 ){
-            return res.status(400).json({error:"password need atleast 6 charactor"})
-           }
-           const passHass = await bycrpt.hash(password,10)
-           const newUser = new User({name,email,password:passHass})
-           await newUser.save();
-          
-               const accessToken = createAccessToken({id: newUser._id})
-               const refreshToken = createRefreshToken({id: newUser._id})
-               res.cookie('refreshToken',refreshToken,{
-                   httpOnly:true,
-                   path:"/user/refresh_token"
-
-               })
-               res.status(201).json({accessToken})
-        
-           
-          
-        }catch(err){
-            res.status(500).json(err.message)
->>>>>>> 1eb56b314e189c431efe0907e8f34acec83ebd97
         }
     
     },
     login: async(req,res)=>{
-<<<<<<< HEAD
      try{
         const {email,password} = req.body
         const user = await User.findOne({email})
@@ -91,48 +59,10 @@ const userCtrl = {
       }catch(err){
         res.status(500).send(err.message)
       }
-=======
-        try{
-            const {email,password} = req.body
-            const user = await User.findOne({email})
-            if(!user) return res.status(400).json({msg:"user doesnot exist"})
-             const isMatch = await  bycrpt.compare(password,user.password)
-             if(!isMatch) return res.status(400).json({msg:"password doesnot match"})
-
-             const accessToken = createAccessToken({id: user._id})
-             const refreshToken = createRefreshToken({id: user._id})
-             res.cookie('refreshToken',refreshToken,{
-                 httpOnly:true,
-                 path:"/user/refresh_token"
-
-             })
-             res.status(201).json({accessToken})
-
-        }catch(err){
-            res.status(500).json({msg:err.message})
-        }
-    },
-    logout:async(req,res) =>{
-        try{
-            res.clearCookie("refreshToken",{  path:"/user/refresh_token"})
-            res.status(200).json({msg:"logged out"})
-        }catch(err){
-            res.status(500).json(err.message)
-        }
-    },
-    getUser:async(req,res)=>{
-       try{
-     const userMatch = await User.findById({_id:req.user.id}).select("-password")
-     res.status(200).json({userMatch})
-       }catch(err) {
-        res.status(500).json({msg:err.message})
-       }
->>>>>>> 1eb56b314e189c431efe0907e8f34acec83ebd97
     },
     refresh_token: (req,res)=>{
        try{
         const rf_token = req.cookies.refreshToken
-<<<<<<< HEAD
         if(!rf_token){
             return  res.status(400).json({err:"please Login or Register"})
         }
@@ -157,17 +87,6 @@ const userCtrl = {
      } catch (error) {
        res.status(500).json({error})
      }
-=======
-        if(!rf_token) return res.status(400).json({msg:"please log in or register"})
-        jwt.verify(rf_token,process.env.REFRESS_TOKEN,(err,user)=>{
-            if(err) return res.status(400).json({msg:"please log in or register"})
-            const accessToken = createAccessToken({id: user.id})
-            res.status(200).json({accessToken})
-        })
-       }catch(err){
-           res.status(500).json(err.message)
-       }
->>>>>>> 1eb56b314e189c431efe0907e8f34acec83ebd97
     }
 }
 
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,19 +1,10 @@
 const router = require("express").Router()
 const userCtrl = require('../controllers/userCtrl')
 const auth = require('../middlewares/auth')
-<<<<<<< HEAD
 router.post("/register", userCtrl.register)
 router.post("/login", userCtrl.login)
 router.get("/logout",userCtrl.logout)
 router.get("/refresh_token", userCtrl.refresh_token)
 router.get("/info", auth ,userCtrl.getUser)
-=======
-
-router.post("/register", userCtrl.register)
-router.post("/login",userCtrl.login)
-router.get("/logout",userCtrl.logout)
-router.get("/refresh_token", userCtrl.refresh_token)
-router.get("/info", auth, userCtrl.getUser)
->>>>>>> 1eb56b314e189c431efe0907e8f34acec83ebd97
 
 module.exports = router
